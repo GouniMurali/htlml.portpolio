8456517033c0b0f5d6d40de0babf52013d34a84e
GouniMurali <151365693+GouniMurali@users.noreply.github.com>
2023-12-26 21:01:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let con=document.querySelector('#container');
+let love=document.querySelector('i');
+
+console.log(love);
+con.addEventListener('dblclick',function(){
+    love.style.transform='translate(-50%,-50%) scale(1)';
+    love.style.opacity=0.8;
+    setTimeout(() => {
+        love.style.opacity=0;
+    }, 1000);
+    setTimeout(() => {
+        love.style.transform='translate(-50%,-50%) scale(0)';
+    }, 3000);
+    
+})
